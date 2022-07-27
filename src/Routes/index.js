16134bc4996458c53883a982d218839add0bde67
makuntz <maira.kuntz@animati.com.br>
2022-07-27 19:16:08 -0300

    first commit

---
+++ src/Routes/index.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+
+const RoutesComponent = () => {
+
+    return(
+        <>
+        <Routes>
+            <Route exact path='/' />
+        </Routes>
+        </>
+    )
+}
+
+export default RoutesComponent
+
+//element={<Home/>}
